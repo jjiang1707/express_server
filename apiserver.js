904177b021d74fdba3fa1fffb2587685ebc9bc16
Jeffery Jiang <jj@192-168-1-131.tpgi.com.au>
2023-04-04 21:52:32 +1000

    initial commit

---
+++ apiserver.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const OktaJwtVerifier = require('@okta/jwt-verifier');
+const oktaJwtVerifier = new OktaJwtVerifier({
+  issuer: 'https://trial-5793640.okta.com/oauth2/default'
+});
+const audience = 'api://default';
+
+//Authenitcation middleware
+const authenticationRequired = async (req, res, next) => {
+  const authHeader = req.headers.authorization || '';
+  const match = authHeader.match(/Bearer (.+)/);
+  if (!match) {
+    return res.status(401).send();
+  }
+
+  try {
+    const accessToken = match[1];
+    if (!accessToken) {
+      return res.status(401, 'Not authorized').send();
+    }
+    req.jwt = await oktaJwtVerifier.verifyAccessToken(accessToken, audience);
+    next();
+  } catch (err) {
+    return res.status(401).send(err.message);
+  }
+};
+
+// Api routes
+app.get('/', (req, res) => {
+  res.send('Home!')
+})
+
+app.get('/api/hello', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.get('/api/whoami', authenticationRequired, (req, res) => {
+    res.send('You got me revealing my secrets!');
+});
+
+
+app
+  .listen(port, () => console.log('API Magic happening on port ' + port));
